456a54c5a89b980f5c920121f87d6cbe2b206a6b
mustafaguldag <guldag.mustafaa@gmail.com>
2021-05-12 14:15:01 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,36 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+];
+
+
+
+
+
+
+function superbowlWin(record){
+  let year;
+  
+  
+  record.find(obj => {
+  
+  
+  if (obj.result === "W"){
+    year = obj.year
+   
+  } else {
+    return undefined
+  }
+  
+})
+return year
+};
+
+
+
+superbowlWin(record);
